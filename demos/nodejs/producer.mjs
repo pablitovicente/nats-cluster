af9b5b39a2d915b0c71fae4da28ca856f3d2f767
Pablo Vicente <pvicente@gmail.com>
2022-08-31 21:18:14 +0200

    Add worker

--- demos/nodejs/producer.mjs
+++ demos/nodejs/producer.mjs
@@ -13,7 +13,8 @@ try {
   let pendingMessages = 10000
   // Publish messages with 1KB payload
   while (pendingMessages) {
-    await jetstreamClient.publish(`data.node_machine_${Math.floor(Math.random() * (pendingMessages) + 1)}`,
+    await jetstreamClient.publish(
+      `data.node_machine_${Math.floor(Math.random() * (pendingMessages) + 1)}`,
       Buffer.from(JSON.stringify({ val: pendingMessages, data: randomBytes(1000) })),
     )
     pendingMessages -= 1
---
+++ demos/nodejs/worker.mjs
@@ -0,0 +1,64 @@
+import { consumerOpts } from 'nats'
+import { setTimeout } from 'timers/promises'
+import http from 'http'
+import { connectNats } from './utils/index.mjs'
+
+let natsConn
+let processedMessages = 0
+const logger = console
+
+const requestListener = (req, res) => {
+  const response = {
+    now: new Date(),
+    processedMessages,
+  }
+  res.writeHead(200)
+  res.end(JSON.stringify(response))
+}
+
+const server = http.createServer(requestListener)
+server.listen(10000)
+
+const drain = async () => {
+  // we want to insure that messages that are in flight
+  // get processed, so we are going to drain the
+  // connection. Drain is the same as close, but makes
+  // sure that all messages in flight get seen
+  // by the iterator. After calling drain on the connection
+  // the connection closes.
+  logger.info('Draining NATS connection...')
+  await natsConn.drain()
+  await natsConn.close()
+  logger.info('Done. Quitting.')
+  process.exit()
+}
+
+process.on('SIGTERM', await drain)
+process.on('SIGINT', await drain)
+
+// Process messages
+try {
+  // connect
+  natsConn = await connectNats({ servers: ['localhost:4222', 'localhost:4223', 'localhost:4224'] })
+  // Create Jetstream client
+  const jetstreamClient = natsConn.jetstream()
+
+  const opts = consumerOpts()
+  opts.queue('machine_data_queue')
+  opts.bind('machine_data', 'machine_data_consumer')
+
+  const sub = await jetstreamClient.subscribe('machine_data_queue', opts)
+  // process messages
+  for await (const m of sub) {
+    console.log(m.data.toString())
+    console.log(m.subject)
+    console.log(`worker got sequence: ${m.seq}`)
+    m.ack()
+    // Simulate slow work
+    await setTimeout(500, '')
+    processedMessages += 1
+  }
+} catch (err) {
+  logger.error('Encountered an error')
+  logger.error(err)
+}
